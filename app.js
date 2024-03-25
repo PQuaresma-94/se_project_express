2c96a17c6546f54d010546ec46ea30d6e7b59379
PQuaresma-94 <pedromquaresma@gmail.com>
2024-03-25 15:41:44 -0700

    Add logger to app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const mainRouter = require("./routes/index");
 const { errorHandler } = require("./middlewares/error-handler");
 
@@ -19,7 +20,9 @@ mongoose
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
+app.use(requestLogger);
 app.use("/", mainRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
