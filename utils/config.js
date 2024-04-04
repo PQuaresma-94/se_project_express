999aa5736d2cceb0040c48afc24d43aa59bd4938
PQuaresma-94 <pedromquaresma@gmail.com>
2024-04-03 18:08:42 -0700

    Remove {} from config.js

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 
-const { JWT_SECRET } = process.env.JWT_SECRET || "super-strong-secret";
+const JWT_SECRET = process.env.JWT_SECRET || "super-strong-secret";
 
 module.exports = { JWT_SECRET };
