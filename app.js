ae9ad937d9c529a3611bd9a4ec30fe2f86fcb5b1
PQuaresma-94 <pedromquaresma@gmail.com>
2024-06-07 09:50:24 -0700

    Remove unecessary code

--- app.js
+++ app.js
@@ -17,13 +17,6 @@ mongoose
   })
   .catch(console.err);
 
-// Remove after passing review
-app.get("/crash-test", () => {
-  setTimeout(() => {
-    throw new Error("Server will crash now");
-  }, 0);
-});
-
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
